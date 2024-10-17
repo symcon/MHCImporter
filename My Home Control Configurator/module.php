5656911a282f99345cacdbbae51e409a442fa0db
Till Brede <tb@symcon.de>
2024-10-17 14:02:34 +0200

    Fix device structure in configurator
    allow / in parenthesis
    add instance ids for devices

--- My Home Control Configurator/module.php
+++ My Home Control Configurator/module.php
@@ -47,9 +47,7 @@ declare(strict_types=1);
             }
             $xml = simplexml_load_string(base64_decode($File), null, LIBXML_NOCDATA);
             $array = json_decode(json_encode($xml), true);
-            $this->SendDebug('Validate', json_encode($array), 0);
             if (!array_key_exists('DeviceAddresses', $array)) {
-                $this->SendDebug('No Address', json_encode($array['DeviceAddresses']), 0);
                 return false;
             }
             return true;
@@ -60,11 +58,12 @@ declare(strict_types=1);
             $xml = simplexml_load_string(base64_decode($File), null, LIBXML_NOCDATA);
             $array = json_decode(json_encode($xml), true);
             $configurator = [];
+            $id = 1;
             foreach ($array['DeviceAddresses']['Sensor'] as $sensor) {
-                $this->createDevice($configurator, $sensor['@attributes'], 'Sensor');
+                $this->createDevice($configurator, $sensor['@attributes'], 'Sensor', $id);
             }
             foreach ($array['DeviceAddresses']['Actuator'] as $actuator) {
-                $this->createDevice($configurator, $actuator['@attributes'], 'Actuator');
+                $this->createDevice($configurator, $actuator['@attributes'], 'Actuator', $id);
             }
             return $configurator;
         }
@@ -74,31 +73,60 @@ declare(strict_types=1);
             $this->UpdateFormField('Configurator', 'values', json_encode($this->createConfiguratorValues($File)));
         }
 
-        private function createDevice(&$configurator, $attributes, String $type)
+        private function createId($path, &$configurator, &$id)
         {
-            $path = explode('/', $attributes['Ref']);
-            // We only want to add the "cateogries" and not the device itself
-            for ($i=0; $i < count($path) -1 ; $i++) {
-                if (!$this->hasID($path[$i], $configurator)) {
-                    $node = [
-                        'name' => $path[$i],
-                        // TODO: We should use the path until here as id to prevent errors by duplicate names
-                        'id' => $path[$i],
-                    ];
-                    if ($i > 0) {
-                        $node['parent'] = $path[$i -1];
-                    // $node['id'] = array_slice($path, 0, $i - 1);
-                    } else {
-                        // $node['id'] = $path[$i];
-                    }
-                    $configurator[] = $node;
+            $pathId =  $this->getID($path, $configurator);
+            if ($pathId != 0) {
+                return $pathId;
+            } elseif (count($path) == 1) {
+                $configurator[] = [
+                    'name' => $path[0],
+                    'id' => ++$id,
+                    'parent' => 0,
+                ];
+                return $id;
+            } else {
+                $parentId = $this->createId(array_slice($path, 0, count($path) - 1), $configurator, $id);
+                $configurator[] = [
+                    'name' => $path[count($path) - 1],
+                    'id' => ++$id,
+                    'parent' => $parentId,
+                ];
+                return $id;
+            }
+        }
+
+
+        private function createDevice(&$configurator, $attributes, String $type, &$id)
+        {
+            $parenthesisCount = 0;
+            $pathStr = $attributes['Ref'];
+            $slashIndices = [0];
+            for ($i=0; $i < strlen($pathStr); $i++) {
+                $char = $pathStr[$i];
+                if ($char == '(') {
+                    $parenthesisCount++;
+                } elseif ($char == ')') {
+                    $parenthesisCount--;
+                } elseif ($char == '/' && $parenthesisCount == 0) {
+                    $slashIndices[] = $i;
                 }
             }
+            $slashIndices[] = strlen($pathStr);
+            $path = [];
+            for ($i=0; $i < count($slashIndices) - 1 ; $i++) {
+                $length = $slashIndices[$i + 1] - $slashIndices[$i];
+                if ($i != 0) {
+                    $length--;
+                }
+                $path[] = substr($pathStr, $i == 0 ? $slashIndices[$i] : $slashIndices[$i] + 1, $length);
+            }
+            $parentId = $this->createId(array_slice($path, 0, count($path) - 1), $configurator, $id);
             $device = [
-                'id' => $attributes['Ref'],
                 'address' => $attributes['Address_hex'],
                 'name' => $path[count($path) - 1],
-                'parent' => $path[count($path) - 2],
+                'parent' => $parentId,
+                'id' => ++$id,
                 'type' => $attributes['Type'],
                 'instanceID' => 0,
             ];
@@ -106,31 +134,35 @@ declare(strict_types=1);
             switch ($type) {
                 case 'Sensor':
                     if ($supported) {
+                        $guid = self::GUID_MAP[$attributes['Type']];
                         $device['create'] = [
-                            'moduleID' => self::GUID_MAP[$attributes['Type']],
+                            'moduleID' => $guid,
                             'location' => array_slice($path, 0, count($path) - 1),
                             'configuration' => [
                                 'DeviceID' => $attributes['Address_hex'],
                             ],
                         ];
+                        $device['instanceID'] = $this->searchDevice($attributes['Address_hex'], $guid);
                     }
                     break;
 
-                case 'Actuator':
-                    if ($supported) {
-                        $create =
+                    case 'Actuator':
+                        if ($supported) {
+                            $guid = self::GUID_MAP[$attributes['Type']];
+                            $create =
                             [
-                                'moduleID' => self::GUID_MAP[$attributes['Type']],
+                                'moduleID' => $guid,
                                 'location' => array_slice($path, 0, count($path) - 1),
                                 'configuration' => [
                                     'DeviceID' => $attributes['Address'],
                                 ],
                             ];
-                        if (array_key_exists('SensorAddr_hex', $attributes)) {
-                            $create['configuration']['ReturnID'] =$attributes['SensorAddr_hex'];
+                            $device['instanceID'] = $this->searchDevice($attributes['Address'], $guid);
+                            if (array_key_exists('SensorAddr_hex', $attributes)) {
+                                $create['configuration']['ReturnID'] =$attributes['SensorAddr_hex'];
+                            }
+                            $device['create'] = $create;
                         }
-                        $device['create'] = $create;
-                    }
                     break;
 
                 default:
@@ -140,13 +172,33 @@ declare(strict_types=1);
             $configurator[] = $device;
         }
 
-        private function hasID($id, $values)
+        private function getID($path, $values)
         {
-            foreach ($values as $element) {
-                if ($element['id'] == $id) {
-                    return true;
+            $getID = function ($parent, $name) use ($values) {
+                foreach ($values as $element) {
+                    if ($element['parent'] == $parent && $element['name'] == $name) {
+                        return $element['id'];
+                    }
+                }
+                return 0;
+            };
+            $id = 0;
+            foreach ($path as $name) {
+                $id = $getID($id, $name);
+            }
+            return $id;
+        }
+
+        private function searchDevice($deviceID, $guid): int
+        {
+            $connectionID = IPS_GetInstance($this->InstanceID);
+            $ids = IPS_GetInstanceListByModuleID($guid);
+            foreach ($ids as $id) {
+                $i = IPS_GetInstance($id);
+                if (IPS_GetProperty($id, 'DeviceID') == $deviceID) {
+                    return $id;
                 }
             }
-            return false;
+            return 0;
         }
     }
